5d3e5c941d69e99441a8784e1f7077e090268511
Raunak Raj <71929976+hackerRaunak@users.noreply.github.com>
2022-05-28 12:30:44 +0530

    ...

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ from pytube import YouTube
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
-from kivy.utils import platform
-
-if platform == "android":
-    from android.permissions import request_permissions, Permission
-    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
 KV = '''
 ScreenManager:
